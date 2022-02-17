2207c0bbb73987995d196aa55522175d1aa67154
faultables <oss@rizaldy.club>
2022-02-17 16:59:50 +0000

    feat: create zone for evlfctry.pro

---
+++ evlfctry.pro.lua
@@ -0,0 +1,9 @@
+ns(_a, "ns1.luadns.net")
+ns(_a, "ns2.luadns.net")
+ns(_a, "ns3.luadns.net")
+ns(_a, "ns4.luadns.net")
+
+alias(_a, "evilfactorylabs.github.io")
+cname("www", "evilfactorylabs.github.io")
+
+txt("_github-challenge-evilfactorylabs", "877b53fd61")
